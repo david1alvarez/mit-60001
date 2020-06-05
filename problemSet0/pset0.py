a6669048bbb83aecb74e126f311921bb575e71ad
David Alvarez <daveyalvarez03@gmail.com>
2020-06-04 17:00:39 -0700

    pset 0

---
+++ problemSet0/pset0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy
+import matplotlib
+x = int(input('Enter number x: '))
+y = int(input('Enter number y: '))
+print(f'X**y = {x**y}')
+print(f'log(x) {numpy.log2(x)}')
